b7eb4134d6ad1f3e2935ec568f5d2e6c6cab3912
高飞 <1083594261@qq.com>
2018-08-24 22:36:00 +0800

    add  saveFile method

--- src/main/java/com/lib/StreamUitl/StreamUitl.java
+++ src/main/java/com/lib/StreamUitl/StreamUitl.java
@@ -72,5 +72,28 @@ public class StreamUitl {
         }
     }
 
+	 /**
+     * 保存文件到磁盘中
+     * @param fileContent  文件内容
+     * @param descPath 目标路径
+     */
+    public static void saveFile(String fileContent,String descPath){
+        BufferedWriter writer = null;
+        try {
+            writer = new BufferedWriter(new FileWriter(descPath));
+            writer.write(fileContent);
+        } catch (IOException e) {
+            throw new DOCError("保存文件失败",e);
+        }finally{
+            try{
+                if(writer!=null){
+                    writer.close();
+                }
+            }catch (IOException e){
+                throw new DOCError("关闭文件流失败",e);
+            }
+        }
+
+    }
 
 }
